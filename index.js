27b8552f5464a5002e2a136be3af11ddcb387c33
Scott Isitt <isitts@hotmail.com>
2022-01-09 23:30:16 -0800

    Add screenshot directory and gitignore the  directory.

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ console.log("Hello.")
 // fetch("https://sampleserver6.arcgisonline.com/arcgis/rest/info")
 
 fetch("https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandClassification/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-130.153%2C44.672%2C-111.300%2C49.883&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json")
-    .then(res => console.log(res))
+    .then(res => res.json())
+    .then(data => console.log(data))
