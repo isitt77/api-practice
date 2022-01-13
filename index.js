eea81e21af98ec4d2ae6d3320f9100489f1dc539
Scott Isitt <isitts@hotmail.com>
2022-01-12 23:19:55 -0800

    Create basic loadIntoTable function as per video two as initial stage to utilize API data.

--- index.js
+++ index.js
@@ -6,31 +6,44 @@ fetch("https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandCl
     method: "GET",
     body: Object.features
 })
-    // 5:45 in https://www.youtube.com/watch?v=cuEtnrL9-H0 
 
     // Fetch json data
     // // .then(res => res.json())
     // .then(data => console.log(data))
 
-
     .then(res => {
         return res.json()
     })
+
     // .then(data => console.log(data.features))
     // .then(data => console.log(data.features[0])) // <-- specific index from array
     // .then(data => console.log(data.features[1].attributes.ParkName))
     // .then(data => document.write(data.features[1].attributes.ParkName))
     .then(data => console.log(data.features[1].geometry.rings[0][0])) // <-- Coordinates long lat
 
+// .then(res => {
+//     if (res.ok) {
+//         console.log("Success!")
+//     } else {
+//         console.log("Not successful")
+//     }
+// })
+// // .then(res => res.json())
+// .then(data => console.log(data))
 
 
+// Video 2 Displaying data on browser
 
-    // .then(res => {
-    //     if (res.ok) {
-    //         console.log("Success!")
-    //     } else {
-    //         console.log("Not successful")
-    //     }
-    // })
-    // // .then(res => res.json())
-    // .then(data => console.log(data))
+const parksApi = "https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandClassification/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-130.153%2C44.672%2C-111.300%2C49.883&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json"
+const table = document.querySelector("table")
+
+async function loadIntoTable(url, table) {
+    const tableHead = document.querySelector("thead")
+    const tableBody = document.querySelector("tbody")
+    const response = await fetch(url)
+    const data = await response.json()
+
+    console.log(data)
+}
+
+loadIntoTable(parksApi, table)
