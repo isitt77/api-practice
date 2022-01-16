b18d1334a61ec95b2577510629ed9a96dfcf746f
Scott Isitt <isitts@hotmail.com>
2022-01-15 22:39:12 -0800

    Commengt out code not being used for testing.

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ console.log("Hello.")
 
 // fetch("https://sampleserver6.arcgisonline.com/arcgis/rest/info")
 
-fetch("https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandClassification/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-130.153%2C44.672%2C-111.300%2C49.883&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json", {
-    method: "GET",
-    body: Object.features
-})
+// fetch("https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandClassification/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-130.153%2C44.672%2C-111.300%2C49.883&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json", {
+//     method: "GET",
+//     body: Object.features
+// })
 
-    // Fetch json data
-    // // .then(res => res.json())
-    // .then(data => console.log(data))
+// Fetch json data
+// // .then(res => res.json())
+// .then(data => console.log(data))
 
-    .then(res => {
-        return res.json()
-    })
+// .then(res => {
+//     return res.json()
+// })
 
-    // .then(data => console.log(data.features))
-    // .then(data => console.log(data.features[0])) // <-- specific index from array
-    // .then(data => console.log(data.features[1].attributes.ParkName))
-    // .then(data => document.write(data.features[1].attributes.ParkName))
-    .then(data => console.log(data.features[1].geometry.rings[0][0])) // <-- Coordinates long lat
+// .then(data => console.log(data.features))
+// .then(data => console.log(data.features[0])) // <-- specific index from array
+// .then(data => console.log(data.features[1].attributes.ParkName))
+// .then(data => document.write(data.features[1].attributes.ParkName))
+// .then(data => console.log(data.features[1].geometry.rings[0][0])) // <-- Coordinates long lat
 
 // .then(res => {
 //     if (res.ok) {
@@ -48,22 +48,22 @@ async function loadIntoTable(url, table) {
     // console.log(data)
 
     let headers = async () => {
-        let header = []
+        // let header = []
         for (let i = 0; i < features.length; i++) {
             const parks = await features[i].attributes.ParkName
             // console.log(header.attributes.ParkName)
             // return parks
-            header.push(parks)
-            // console.log(parks)
+            // header.push(parks)
+            console.log(parks)
         }
         // features[0].attributes.ParkName
         // console.log(header)
         // return header.join("")
-        return header
+        // return header
         // console.log(parksArr)
         // return parksArr
     }
-    // headers()
+    headers()
 
     let rows = features[0].geometry.rings[0][0]
 
@@ -72,12 +72,12 @@ async function loadIntoTable(url, table) {
     tableBody.innerHTML = ""
 
     // Populate headers 
-    for (let headerText of headers) {
-        const headerElement = document.createElement("th")
+    // for (let headerText of headers) {
+    //     const headerElement = document.createElement("th")
 
-        headerElement.textContent = headerText
-        tableHead.querySelector("tr").appendChild(headerElement)
-    }
+    //     headerElement.textContent = headerText
+    //     tableHead.querySelector("tr").appendChild(headerElement)
+    // }
 
     // Populate headers v2
     // for (let headerText of headers) {
