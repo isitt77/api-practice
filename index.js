750cd4f897fd8eecf648f9d1738095460558eb64
Scott Isitt <isitts@hotmail.com>
2022-01-17 22:44:00 -0800

    Use .map() method to extract park names from their respective arrayys and put them into one new array.

--- index.js
+++ index.js
@@ -20,20 +20,25 @@ function fetchData() {
         .then(data => {
             // console.log(data.features)
             // const parkName = data.features[0].attributes.ParkName
-            const features = data.features
+            // const features = data.features
 
             // document.querySelector("#app")
             //     .insertAdjacentHTML("afterbegin", `<h1>${parkName}</h1>`)
 
-            for (let i = 0; i < features.length; i++) {
-                const feature = data.features[i];
-                console.log(feature.attributes.ParkName)
-                const park = feature.attributes.ParkName
-                document.querySelector("#app")
-                    .insertAdjacentHTML("afterbegin", `<h1>${park}</h1>`)
-            }
+            // My loop
+            // for (let i = 0; i < features.length; i++) {
+            //     const feature = data.features[i];
+            //     console.log(feature.attributes.ParkName)
+            //     const park = feature.attributes.ParkName
+            //     document.querySelector("#app")
+            //         .insertAdjacentHTML("afterbegin", `<h1>${park}</h1>`)
+            // }
 
-
+            // Video 3's loop (.map) 
+            const features = data.features.map(f => {
+                return f.attributes.ParkName
+            })
+            console.log(features)
 
         })
         .catch(err => {
