e66a22b685161f64c944a1f8a967655e5ad34ccc
Scott Isitt <isitts@hotmail.com>
2022-01-22 00:46:23 -0800

    Change method to accommodate fetch API request.

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ const app = Vue.createApp({
         printFeatures() {
             console.log(this.tableName)
             return this.tableName
+        },
+        created() {
+            fetch("https://services5.arcgis.com/4LKAHwqnBooVDUlX/arcgis/rest/services/LandClassification/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-130.153%2C44.672%2C-111.300%2C49.883&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json")
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("Response not ok.")
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    // Video 3's loop (.map) 
+                    const features = data.features.map(f => {
+                        // console.log(f.attributes)
+                        return `<p>${f.attributes.OBJECTID}. ${f.attributes.ParkName}</p>`
+                    }).join(" ")
+                    // console.log(features)
+                    document.querySelector("#app")
+                        .insertAdjacentHTML("afterbegin", features)
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
     }
 })
